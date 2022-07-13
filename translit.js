e03810f70158602ca2fe437f454151e62be8a878
VVS693 <109098953+VVS693@users.noreply.github.com>
2022-07-13 20:00:31 +0300

    version 1.1)

--- translit.js
+++ translit.js
@@ -10,42 +10,45 @@ const eng = [
     'a', 'b', 'v', 'g', 'd', 'e', 'e', 'zh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o',
     'p', 'r', 's', 't', 'u', 'f', 'h', 'ts', 'ch', 'sh', 'y', 'e', 'yu', 'ya',
     'A', 'B', 'V', 'G', 'D', 'E', 'E', 'ZH', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O',
-    'P', 'R', 'S', 'T', 'U', 'F', 'H', 'TS', 'CH', 'SH', 'Y', 'E', 'YU', 'YA'
+    'P', 'R', 'S', 'T', 'U', 'F', 'H', 'TS', 'CH', 'SH', 'Y', 'E', 'YU', 'YA', 'Ts', 'Ch', 'Sh', 'Y', 'E', 'Yu', 'Ya'
             ]
 
     
-
-    
-    textIn.oninput = function() {
-      
-    let message = textIn.value
-  
+  let poleTextIn = document.getElementById("textIn");
+  poleTextIn.addEventListener("input", () => {
+    let message = poleTextIn.value;
     let messageTranslit = "";
-
     let messageArray = Array.from(message);
-
     for (let i = 0; i < messageArray.length; i++) {
       let letter = messageArray[i];
-
-      if (eng.includes(letter)) {
-        for (let i1 = 0; i1 < eng.length; i1++) {
-          if (letter === eng[i1]) {
-            messageTranslit = messageTranslit + rus[i1];
-          }
-        }
-      } else if (rus.includes(letter)) {
-        for (let i2 = 0; i2 < rus.length; i2++) {
-          if (letter === rus[i2]) {
-            messageTranslit = messageTranslit + eng[i2];
-          }
-        }
+      if (rus.includes(letter)) {
+        messageTranslit = messageTranslit + eng[rus.indexOf(letter)];
       } else if (letter === "ъ" || letter === "ь") {
         messageTranslit = messageTranslit;
       } else {
         messageTranslit = messageTranslit + letter;
       }
     }
-
     textOut.innerHTML = messageTranslit;
+  });
 
-  };
+
+//   let poleTextIn = document.getElementById('textIn')
+//   function translit () {
+//   let message = poleTextIn.value;
+//   let messageTranslit = '';
+//   let messageArray = Array.from(message);
+//   for (let i = 0; i < messageArray.length; i++) {
+//     let letter = messageArray[i];
+//     if (rus.includes(letter)) {
+//       messageTranslit = messageTranslit + eng[rus.indexOf(letter)];
+//     } else if (letter === "ъ" || letter === "ь") {
+//       messageTranslit = messageTranslit;
+//     } else {
+//       messageTranslit = messageTranslit + letter;
+//     }
+//   }
+//   textOut.innerHTML = messageTranslit;
+// }
+// poleTextIn.addEventListener('input', translit)
+  
